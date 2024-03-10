09e71ddda0db08814db59e923ba99a21fbd9a5a6
5kind <shikabane320@gmail.com>
2024-03-10 16:57:28 +0800

    scripts: Add kali-nethunter-kernel.sh

---
+++ scripts/patch/kali-nethunter-kernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Patches source: kalilinux
+# Shell author:   5kind <shikabane320@gmail.com>
+# 20240310
+
+apply_nethunter_patches_except() {
+    for patch in $NETHUNTER_PATCHES/*.patch; do
+        case " $@ " in
+            *" $(basename $patch) "*)
+                continue    ;;
+        esac
+        patch -p1 < $patch
+    done
+}
+
+apply_nethunter_patches_except add-rtl8188eus-to-rtl8xxxu-drivers-4.14.patch

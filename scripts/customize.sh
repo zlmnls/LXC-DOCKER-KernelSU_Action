43c1a073c84c31a7bf5113eeca2fdfbe901801ab
5kind <shikabane320@gmail.com>
2024-03-10 17:16:15 +0800

    scripts: Add customize.sh

---
+++ scripts/customize.sh
@@ -0,0 +1,7 @@
+CONFIG_FILE=$1
+scripts/config --file $CONFIG_FILE --enable CONFIG_BINFMT_MISC
+scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-KernelSU"
+
+for script in $(dirname "$(realpath "$0")")/customize/*.sh; do
+    . "$script"
+done
---
+++ scripts/customize/check-kernel-config.sh
@@ -0,0 +1,1 @@
+bash $GITHUB_WORKSPACE/scripts/check-kernel-config/customize.sh $CONFIG_FILE -w

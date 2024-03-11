7edd25b6fc7c3bbf0299d2815c339e0216851f30
5kind <shikabane320@gmail.com>
2024-03-11 11:37:52 +0800

    config: set CONFIG_LOCALVERSION

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,6 +1,6 @@
 CONFIG_FILE=$1
 scripts/config --file $CONFIG_FILE --enable CONFIG_BINFMT_MISC
-scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-KernelSU"
+scripts/config --file $CONFIG_FILE --set-str CONFIG_LOCALVERSION "-Marisa"
 
 for script in $(dirname "$(realpath "$0")")/customize/*.sh; do
     . "$script"
